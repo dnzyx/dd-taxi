251834d980ff31742bc0aefc3bbd5ed008b740a5
dnzyx <dndszyx@qq.com>
2017-10-21 13:47:48 +0800

    add OnclickListener

--- app/src/main/java/com/leanring/dnzyx/dd_taxi/bootom.java
+++ app/src/main/java/com/leanring/dnzyx/dd_taxi/bootom.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 /**
  * Created by Lenovo on 2017/10/19.
@@ -12,8 +13,41 @@ import android.view.ViewGroup;
 
 public class bootom extends Fragment
 {
+    View bottom;
+    ImageButton talk,server,me;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.bottom,container,true);
+
+       bottom = inflater.inflate(R.layout.bottom, null);
+        talk= (ImageButton)bottom.findViewById(R.id.talk);
+        server=(ImageButton)bottom.findViewById(R.id.server);
+        me=(ImageButton)bottom.findViewById(R.id.me);
+        return bottom;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+     talk.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+
+         }
+     });
+
+        server.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+      me.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/leanring/dnzyx/dd_taxi/location.java
+++ app/src/main/java/com/leanring/dnzyx/dd_taxi/location.java
@@ -5,14 +5,32 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by Lenovo on 2017/10/19.
  */
 
 public class location extends Fragment {
+
+    View locat;
+    Button call_car;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        locat=inflater.inflate(R.layout.location,null);
+        call_car=(Button) locat.findViewById(R.id.call_car);
         return inflater.inflate(R.layout.location,container,true);
+    }
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        call_car.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
